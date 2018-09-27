80b2f53baad487f2b2b5f13e3835b6eaed776c57
Colin Knebl <colin.knebl@outlook.com>
2018-09-27 08:47:01 -0400

    updated scripts in package.json and removed clientJS folder

--- clientJS/src/components/header/navbar/navbar.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Navbar from './navbar';
-
-it('Navbar renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Navbar />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
--- clientJS/src/components/sidebar/sidebar.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Sidebar from './sidebar';
-
-it('Sidebar renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Sidebar />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
