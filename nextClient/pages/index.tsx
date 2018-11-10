51021699f8b919fa2b748af34da0656e0d9693a4
Colin Knebl <colin.knebl@outlook.com>
2018-11-10 08:49:13 -0500

    initial next config and routing seems to be working correctly

---
+++ nextClient/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Index extends React.Component {
+    render() {    
+        return (
+            <div>
+                Hello Next.js
+            </div>
+        )
+    }
+}
+
+export default Index;
